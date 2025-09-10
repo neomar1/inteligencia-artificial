ba93214469080aa9aa09daf81cd53d3bdb3e91bf
neomar1 <159184966+neomar1@users.noreply.github.com>
2025-09-10 11:17:29 -0300

    10/09

--- script.js
+++ script.js
@@ -8,27 +8,46 @@ const perguntas = [
 {
     enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
     alternativas:[
-        "Isso é assustador!",
-        "Alternativa 2",
+        {
+        texto:"Isso é assustador!",
+        afirmacao:"afirmação"
+        },
+        {
+        texto:"Isso é maravilhoso!",
+        afirmacao:"afirmação"
+        }
+
     ]
 },
 
 {
     
-        enunciado: "pergunta 2",
+        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial (IA), uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
         alternativas:[
-            "Alternativa 1",
-            "Alternativa 2",
+            {
+            texto:"Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+            afirmacao:"afirmação"
+            },
+            {
+            texto:"Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+            afirmacao:"afirmação"
+            }
         ]
     },
     
 
     {
     
-        enunciado: "pergunta 3",
+        enunciado: "Após a elaboração do trabalho, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
         alternativas:[
-            "Alternativa 1",
-            "Alternativa 2",
+            {
+            texto:"Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+            afirmacao:"afirmação"
+            },
+            {
+            texto:"Me preocupo com as pessoas que perderão seus empregos para máquinas e defendo a importância de proteger os trabalhadores.",
+            afirmacao:"afirmação"
+            }
         ]
     },
     
@@ -36,20 +55,32 @@ const perguntas = [
 
     {
     
-        enunciado: "pergunta 4",
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
         alternativas:[
-            "Alternativa 1",
-            "Alternativa 2",
+            {
+            texto:"Criar uma imagem utilizando uma plataforma de design como o Paint.",
+            afirmacao:"afirmação"
+            },
+            {
+            texto:"Criar uma imagem utilizando um gerador de imagem de IA.",
+            afirmacao:"afirmação"
+            }
         ]
     },
     
 
     {
     
-        enunciado: "pergunta 5",
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda de uma IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
         alternativas:[
-            "Alternativa 1",
-            "Alternativa 2",
+            {
+            texto:"Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+            afirmacao:"afirmação"
+            },
+            {
+            texto:"O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+            afirmacao:"afirmação"
+            }
         ]
     },
 ]
@@ -60,5 +91,16 @@ let perguntaAtual;
 function mostraPergunta(){
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
-    
+    mostraAlternativa();
+
 }
+
+function mostraAlternativa(){
+    for(const alternativa of perguntaAtual.alternativa){
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa;
+    caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mopstraPergunta();
